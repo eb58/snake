20757c519c6c0c6610e2171990567a1a8ae8f4eb
eb58 <erich.brandl@googlemail.com>
2020-01-30 19:03:07 +0100

    Update snakeview.js

--- public_html/js/snakeview.js
+++ public_html/js/snakeview.js
@@ -1,6 +1,4 @@
-/* global Raphael */
-
-var snakeview = function (w, h) {
+const snakeview = (w, h) => {
   const scale = 10;
   const col = {
     background: '#aaa',
@@ -12,9 +10,9 @@ var snakeview = function (w, h) {
   const paper = Raphael(10, 10, w * scale, h * scale);
   paper.rect(0, 0, w * scale, h * scale).attr({fill: col.background, stroke: col.backgroundFrame, 'stroke-width': 8});
 
-  const draw = (snake, hit) => {
-    var head = snake.getArr()[0];
-    var tail = snake.getArr()[snake.getArr().length - 1];
+  const drawSnake = (snake, hit) => {
+    const head = snake.getArr()[0];
+    const tail = snake.getArr()[snake.getArr().length - 1];
     paper.circle(tail.x * scale, tail.y * scale, 4).attr({fill: col.background, stroke: col.background, 'stroke-width': 4});
     _.initial(snake.getArr()).map(function (v) {
       paper.circle(v.x * scale, v.y * scale, 3).attr({fill: col.snake, stroke: col.snake, 'stroke-width': 3});
@@ -24,10 +22,10 @@ var snakeview = function (w, h) {
     }
   }
 
-  const drawFruit = (fruit) => paper.circle(fruit.x * scale, fruit.y * scale, 3).attr({fill: col.fruit, stroke: col.fruit, 'stroke-width': 3});
+  const drawFruit = fruit => paper.circle(fruit.x * scale, fruit.y * scale, 3).attr({fill: col.fruit, stroke: col.fruit, 'stroke-width': 3});
 
   return {
-    draw,
+    drawSnake,
     drawFruit
   };
 };
